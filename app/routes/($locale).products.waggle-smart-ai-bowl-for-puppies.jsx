b93ac5b63601fedd21fea94d6fdf0b3b99bfd471
nishidpatel25 <nishid@synergytechs.net>
2025-06-16 18:03:22 +0530

    Fetch Prodct data Dyanmically

---
+++ app/routes/($locale).products.waggle-smart-ai-bowl-for-puppies.jsx
@@ -0,0 +1,92 @@
+import PetSafetyGrid from '~/components/PetSafetyGrid';
+import PotionControl from '~/components/PotionControl';
+import SmartPetBowlShowcase from '~/components/SmartPetBowlShowcase';
+import FrequentlyBoughtTogether from '~/components/FrequentlyBoughtTogether';
+import WaggleSteps from '~/components/WaggleSteps';
+import TestimonialsSection from '~/components/TestimonialsSection';
+import FAQsection from '~/components/FAQsection';
+import {useLoaderData} from 'react-router';
+import {FETCH_PRODUCT_USING_HANDLE,METOBJECT_DATA_QUERY} from '~/utils/product-query';
+
+export async function loader({context}) {
+  const SHOPIFY_DOMAIN = context.env.PUBLIC_STORE_DOMAIN;
+  const STOREFRONT_API_VERSION = context.env.STOREFRONT_API_VERSION;
+  const STOREFRONT_ACCESS_TOKEN = context.env.PUBLIC_STOREFRONT_API_TOKEN;
+
+  const product = await fetchProductByHandle(
+    SHOPIFY_DOMAIN,
+    STOREFRONT_API_VERSION,
+    STOREFRONT_ACCESS_TOKEN,
+  );
+
+  const instructionMetaobjectData = await getInstructionMetaobjectData({ context })  
+
+  return {
+    product,
+    instructionMetaobjectData,
+  };
+}
+
+async function getInstructionMetaobjectData({ context } = {}) {
+    if (!context?.storefront) {
+      throw new Error("Missing storefront context.");
+    }
+  
+    const { storefront } = context;
+  
+    const instructionMetaobjectData = await storefront.query(
+      METOBJECT_DATA_QUERY,
+      {
+        variables: { type: 'product_instruction' },
+      },
+    );
+  
+    return { instructionMetaobjectDatas: instructionMetaobjectData };
+  }
+  
+
+async function fetchProductByHandle(
+  SHOPIFY_DOMAIN,
+  STOREFRONT_API_VERSION,
+  STOREFRONT_ACCESS_TOKEN,
+) {
+  const query = FETCH_PRODUCT_USING_HANDLE;
+
+  const response = await fetch(
+    `https://${SHOPIFY_DOMAIN}/api/${STOREFRONT_API_VERSION}/graphql.json`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-Shopify-Storefront-Access-Token': STOREFRONT_ACCESS_TOKEN,
+      },
+      body: JSON.stringify({query}),
+    },
+  );
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    throw new Error(`Shopify API error: ${response.status} ${errorText}`);
+  }
+
+  const product = await response.json();
+  return product;
+}
+
+export default function CustomPage() {
+  const ProductData = useLoaderData();
+    // console.log(ProductData);
+    
+  
+  return (
+    <div>
+      <PotionControl ProductData={ProductData} />
+      <SmartPetBowlShowcase />
+      <FrequentlyBoughtTogether />
+      <WaggleSteps />
+      <TestimonialsSection />
+      <FAQsection />
+      <PetSafetyGrid />
+    </div>
+  );
+}
---
+++ app/utils/product-query.jsx
@@ -0,0 +1,49 @@
+export const FETCH_PRODUCT_USING_HANDLE = `
+    query ProductTitle {
+        product(handle: "waggle-smart-ai-bowl-for-puppies") {
+            title
+            description
+            images(first:10){
+                edges{
+                    node{
+                    url
+                    }
+                }
+            }
+            variants(first:10){
+                edges{
+                    node{
+                    price{
+                        amount
+                    }
+                    compareAtPrice{
+                        amount
+                    }
+                    }
+                }
+            }
+            iconWithDiscription: metafield(namespace:"custom", key:"description_with_icon") {
+                    value
+            }
+            iconWithDiscriptionLabel: metafield(namespace:"custom", key:"description_with_icon_label") {
+                    value
+            }
+        }
+    }
+`;
+
+
+export const METOBJECT_DATA_QUERY = `#graphql
+  query MetaobjectData($type: String!) {
+    metaobjects(type:$type, first: 250) {
+        edges {
+            node {
+                fields {                    
+                    key
+                    value
+                }
+            }
+        }
+    }
+  }
+`;
